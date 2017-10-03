557a801d747a637f42076298c794343afe45f0f8
Encho Enevski <encho.enevski@gmail.com>
2017-10-03 17:30:05 +0300

    Added Appraisal System project structure

---
+++ AppraisalSystem.Web/Account/Login.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.Security;
+
+namespace AppraisalSystem.Web.Account
+{
+    public partial class Login : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void LogIn(object sender, EventArgs e)
+        {
+            if (Membership.ValidateUser(UserName.Text, Password.Text))
+            {
+                FormsAuthentication.RedirectFromLoginPage(UserName.Text, false);
+            }
+            else
+            {
+                FailureText.Text = "Invalid login!";
+            }
+        }
+    }
+}
